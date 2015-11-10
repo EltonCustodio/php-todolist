4de01b08cd5348de6bccc56d1a17d538476fab08
Elton Custodio Ribeiro <elton_custodio@live.com>
2015-11-10 22:34:40 +0000

    List in tasks

--- api/index.php
+++ api/index.php
@@ -7,9 +7,18 @@ $app->get('/', function() use ( $app ) {
     echo "Welcome to REST API";
 });
 
-$app->get('/hello/:name', function($name) use ( $app ) {
-    echo "Hi $name, welcome to the REST API's";
+$app->get('/tasks', function() use ( $app ) {
+    $tasks = getTasks();
+    echo json_encode($tasks);
 });
+
+function getTasks(){
+     $tasks = array(
+        array('id'=>1,'description'=>'Learn Rest','done'=>false),
+        array('id'=>2,'description'=>'Learn JavaScript','done'=>false)
+    );
+    return $tasks;
+}
 
 $app->run();
 ?>
